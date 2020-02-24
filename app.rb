3b9110563068ef48a2357c7d94beac064f0c9ce3
CSylliaasen <curtis.sylliaasen@kellogg.northwestern.edu>
2020-02-24 05:43:11 +0000

    Final Commit

--- app.rb
+++ app.rb
@@ -14,12 +14,16 @@ get "/" do
 end
 
 get "/news" do
+    if params["q"] != "" then
     results = Geocoder.search(params["q"])
     @location = results.first.city
+    @locationState = results.first.state
+    @locationCountry = results.first.country
     @lat_long = results.first.coordinates # => [lat, long]
     @forecast = ForecastIO.forecast(@lat_long[0],@lat_long[1]).to_hash
     url = "https://newsapi.org/v2/top-headlines?country=#{results.first.country_code}&apiKey=0aa4a46912d945e49daf6aad7e887036"
     @news = HTTParty.get(url).parsed_response.to_hash
     @locationSet = true
+    end
     view "ask"
 end
